e3708a3bcc811e0a66b8836402f2bb38c59a2a96
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-01-28 22:14:26 +0530

    Add routing and layout components; implement HomePage, CreatePost, Posts, and Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
+import Sidebar from './components/common/SideBar'
 
 function App() {
   
   return (
     <>
-      <p>Hello</p>
+      <div className='flex max-w-6xl mx-auto'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </div>
     </>
   )
 }
